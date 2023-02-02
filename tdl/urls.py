f8e626328de7f73af691a3fe7b0f4b85132f5eed
Hyunook Kwon <hyunook23@gmail.com>
2023-02-02 16:21:47 -0500

    CRUD operation on Task

--- tdl/urls.py
+++ tdl/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("complete/<str:pk>", views.complete, name="complete"),
+    path("delete/<str:pk>", views.delete, name="delete"),
 ]
--- tdl/views.py
+++ tdl/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import date
+from .models import Task
 
 # Create your views here.
 
 
 def index(request):
     today = date.today()
-
+    tasks = Task.objects.all()
     if request.method == "POST":
-        pass
+        task = Task(title=request.POST["title"])
+        task.save()
+
+        return redirect("index")
 
     context = {
         "today": today,
+        "tasks": tasks,
     }
+
     return render(request, "tdl/index.html", context)
+
+
+def complete(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.complete:
+        task.complete = False
+    else:
+        task.complete = True
+    task.save()
+
+    return redirect("index")
+
+
+def delete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return redirect("index")
