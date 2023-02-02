ad9af4506236ca3065d61335a98c33917c9a00f1
Hyunook Kwon <hyunook23@gmail.com>
2023-02-02 14:52:54 -0500

    template initial and static

--- tdl/views.py
+++ tdl/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from datetime import date
 
 # Create your views here.
 
 def index(request):
-    context = {}
-    return render(request, "main.html", context)
+
+    today = date.today()
+
+    context = {
+        "today": today,
+    }
+    return render(request, "tdl/index.html", context)
 
