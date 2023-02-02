c9c8ef71e7578d956b07e1a873ec4baa7269bbba
Hyunook Kwon <hyunook23@gmail.com>
2023-02-02 15:03:25 -0500

    intial Task model

--- tdl/models.py
+++ tdl/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=254)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["complete", "-created"]
+
+    def __str__(self):
+        return self.title
